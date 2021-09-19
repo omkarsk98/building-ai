b13758eb934774edfc73bd1fc4e9c57ce624a510
Omkar <omkarsk@asort.com>
2021-09-19 22:13:00 +0530

    Why AI matters

---
+++ Chapter1/Why AI Matters/Greet (intermediate).py
@@ -0,0 +1,11 @@
+# you can define your own functions
+def greet(name):
+    print("Welcome " + name + "!")
+
+# go ahead and enter your own name in variable 'name' below to get a warm welcome from us.
+# to pass the test, you should change the name to "to Building AI" (note the word 'to')
+def main():
+    name = "to Building AI"
+    greet(name)
+
+main()
